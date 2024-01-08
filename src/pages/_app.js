9ec36e809ccdd440ed39a63c2ab6002ac1ee3b52
Kunat Kamprommapirak <kunat.kamprommapirak@gmail.com>
2024-01-09 05:04:37 +0700

    add font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,13 @@
 import Navbar from "@/component/navbar";
 import * as React from 'react'
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, omitThemingProps } from "@chakra-ui/react";
+import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;700&display=swap" />
     return (
         <ChakraProvider>
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;700&display=swap" rel="stylesheet" />
             <Component {...pageProps} />
         </ChakraProvider>
     )
