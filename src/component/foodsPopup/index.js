bb18ba03fb1b0fc160001b152f64073649e7610d
Kunat Kamprommapirak <kunat.kamprommapirak@gmail.com>
2024-01-15 20:02:24 +0700

    ีupdate ui manage foods

--- src/component/foodsPopup/index.js
+++ src/component/foodsPopup/index.js
@@ -1,6 +1,10 @@
 import {
     Button,
+    FormControl,
+    FormHelperText,
+    FormLabel,
     HStack,
+    IconButton,
     Modal,
     ModalBody,
     ModalCloseButton,
@@ -8,37 +12,167 @@ import {
     ModalFooter,
     ModalHeader,
     ModalOverlay,
+    NumberInput,
+    NumberInputField,
+    SimpleGrid,
+    Stack,
     Text,
+    Textarea,
     VStack,
     useDisclosure,
 } from "@chakra-ui/react";
 import React, { useRef, useEffect, useState } from "react";
 import * as Constants from "@/constant";
 import ButtonTheme from "@/component/butonTheme";
+import InputTheme from "@/component/inputTheme";
+import InputNumberTheme from "@/component/inputNumberTheme";
+import { AddIcon, CloseIcon } from "@chakra-ui/icons";
 
-function FoodsPopup({ isOpen, onOpen, onClose, config}) {
-    useEffect(()=>{
-        console.log("config = ",config)
-    },[config])
+function FoodsPopup({ isOpen, onOpen, onClose, config }) {
+    const requestDefualt = {
+        _id: "",
+        name: "",
+        calorie: "",
+        ingredients: [],
+    };
+    const [request, setRequest] = useState(requestDefualt);
+
+    useEffect(() => {
+        console.log("config = ", config);
+        if (!(Object.keys(config.data).length === 0)) {
+            setRequest(config.data);
+        } else {
+            setRequest(requestDefualt);
+        }
+    }, [config]);
+
+    const handleChange = (property) => (event) => {
+        var value = event.target.value;
+        setRequest((prevRequest) => ({
+            ...prevRequest,
+            [property]: value,
+        }));
+    };
+
+    const handleChangeValue = (property) => (value) => {
+        setRequest((prevRequest) => ({
+            ...prevRequest,
+            [property]: value,
+        }));
+    };
+
+    const handleChangeIngredients = (property, index, event) => {
+        const updatedIngredients = [...request.ingredients];
+        updatedIngredients[index][property] = event.target.value;
+      
+        setRequest({
+          ...request,
+          ingredients: updatedIngredients,
+        });
+    };
+
+    const handleDeleteIngredient = (index) => {
+        const updatedIngredients = [...request.ingredients];
+        updatedIngredients.splice(index, 1);
+      
+        setRequest({
+          ...request,
+          ingredients: updatedIngredients,
+        });
+    };
+
+    const handleAddIngredient = () => {
+        const newIngredient = { name: "", amount: "", unit: "" };
+      
+        setRequest({
+          ...request,
+          ingredients: [...request.ingredients, newIngredient],
+        });
+    };
+      
+    
+
     return (
         <>
-            <Modal isCentered isOpen={isOpen} onClose={onClose} size={'3xl'}>
+            <Modal isCentered isOpen={isOpen} onClose={onClose} size={"3xl"}>
                 <ModalOverlay bg="blackAlpha.300" backdropFilter="blur(2px)" />
                 <ModalContent>
-                    <ModalHeader>{config.title}</ModalHeader>
+                    <ModalHeader color={Constants.colorTheme600}>
+                        {config.title}
+                    </ModalHeader>
                     {/* <ModalCloseButton /> */}
                     <ModalBody>
-                        <VStack spacing={2} align="stretch">
-                            <Text>{JSON.stringify(config.data)}</Text>
+                        <VStack spacing={2} align="stretch" mb={5}>
+                            {/* <Text>{JSON.stringify(config.data)}</Text> */}
+                            <Text>{JSON.stringify(request)}</Text>
+                            <FormControl isRequired>
+                                <FormLabel color={Constants.colorTheme600}>
+                                    ชื่ออาหาร
+                                </FormLabel>
+                                <InputTheme
+                                    value={request.name}
+                                    onChange={handleChange("name")}
+                                />
+                            </FormControl>
+                            <FormControl isRequired>
+                                <FormLabel color={Constants.colorTheme600}>
+                                    พลังงานทั้งหมด (kcal)
+                                </FormLabel>
+                                <InputNumberTheme
+                                    min={0}
+                                    value={request.calorie}
+                                    onChange={handleChangeValue("calorie")}
+                                >
+                                    <NumberInputField />
+                                </InputNumberTheme>
+                            </FormControl>
+                            <Stack flexDirection={"row"} alignItems={'center'}>
+                                <Text color={Constants.colorTheme600}>
+                                    วัตถุดิบ
+                                </Text>
+                                <IconButton
+                                    size="xs"
+                                    colorScheme='green'
+                                    icon={<AddIcon></AddIcon>}
+                                    variant="outline"
+                                    onClick={() => handleAddIngredient()}
+                                >
+                                    
+                                </IconButton>
+                            </Stack>
+                            {
+                                request.ingredients.map((item,index)=>{
+                                    return (
+                                        <Stack key={'ingredients_'+index} flexDirection={"row"} alignItems={'center'}>
+                                            <InputTheme placeholder={"ชื่อวัตถุดิบ"} value={request.ingredients[index].name} onChange={(e) => handleChangeIngredients("name", index, e)}/>
+                                            <InputTheme placeholder={"จำนวน"} value={request.ingredients[index].amount} onChange={(e) => handleChangeIngredients("amount", index, e)}/>
+                                            <InputTheme placeholder={"หน่วย"} value={request.ingredients[index].unit} onChange={(e) => handleChangeIngredients("unit", index, e)}/>
+                                            <IconButton
+                                                size="xs"
+                                                colorScheme='red'
+                                                icon={<CloseIcon></CloseIcon>}
+                                                variant="outline"
+                                                onClick={() => handleDeleteIngredient(index)}
+                                            ></IconButton>
+                                        </Stack>
+                                    )
+                                })
+                            }
+                            
                         </VStack>
-                        
+
                     </ModalBody>
                     <ModalFooter>
                         <HStack>
-                            <ButtonTheme onClick={onClose} customColor={'green'}>
+                            <ButtonTheme
+                                onClick={onClose}
+                                customColor={"green"}
+                            >
                                 {config.confirmText}
                             </ButtonTheme>
-                            <ButtonTheme onClick={onClose} customColor={'red'}>ปิด</ButtonTheme>
+                            <ButtonTheme onClick={onClose} customColor={"red"}>
+                                ปิด
+                            </ButtonTheme>
                         </HStack>
                     </ModalFooter>
                 </ModalContent>
--- src/component/inputNumberTheme/index.js
+++ src/component/inputNumberTheme/index.js
@@ -1,12 +1,12 @@
 import * as Constants from "@/constant";
-import { Box, Input, Text, VStack } from "@chakra-ui/react";
+import { Box, NumberInput, Text, VStack } from "@chakra-ui/react";
 import React, { useRef, useEffect } from "react";
 
-function InputTheme(props) {
-    return <Input
+function InputNumberTheme(props) {
+    return <NumberInput
         focusBorderColor={Constants.colorTheme600}
         {...props} 
     />;
 }
 
-export default InputTheme;
+export default InputNumberTheme;
